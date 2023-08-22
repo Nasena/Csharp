6e4003debe950ea01bf2fb71f4ca4c1562783f80
Nas <1bestany@gmail.com>
2023-08-22 13:14:20 +0400

    added task23

---
+++ task23/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
+
+Console.WriteLine("Введите Число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+
+for (int i = 1; i <= numberN; i++)
+{
+    result = Math.Pow(i, 3);
+    Console.WriteLine(result);
+}
+
+for (int i = 1; i >= numberN; i--)
+{
+    result = Math.Pow(i, 3);
+    Console.WriteLine(result);
+}
