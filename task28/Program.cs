c79af219556d163c934b69e52bcb1d96fb391571
Nas <1bestany@gmail.com>
2023-08-25 12:44:26 +0400

    added task28

---
+++ task28/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+ */
+
+ Console.WriteLine("Введите Число N: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Multiplication(int number)
+{
+    int multiplication = 1;
+    for (int i = 1; i <= userNumber; i++)
+{
+multiplication = multiplication*i;
+}
+    
+    return multiplication;
+}
+
+Console.WriteLine(Multiplication(userNumber));
