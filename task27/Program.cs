c71ee033719a2fec580871dc8437b5e6d65ff00d
Nas <1bestany@gmail.com>
2023-08-31 11:09:57 +0400

    added task27

---
+++ task27/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+Console.WriteLine("Введите Число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number%10;
+        number /= 10;
+    }
+    
+    return sum;
+}
+
+Console.WriteLine($"Сумма чисел числа {userNumber} равна - {SumOfNumber(userNumber)}");
+
