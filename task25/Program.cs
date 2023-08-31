0293aee73a2be2695a4a32aed07aec73506400da
Nas <1bestany@gmail.com>
2023-08-31 11:09:22 +0400

    added task 25

---
+++ task25/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите числа A и B: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int ProductOfNumber(int a, int b)
+{
+     int product = 1;
+     {
+          for (int i = 1; i <= b; i++)
+          {
+               product = product * a;
+          }
+     }
+     return product;
+}
+
+if (numberB > 0)
+{
+     System.Console.WriteLine($"Число {numberA} в степени {numberB} равно - {ProductOfNumber(numberA, numberB)}");
+}
+else 
+{
+     Console.WriteLine("Вы ввели отрицательную степень");
+}
+
