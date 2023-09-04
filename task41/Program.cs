c8481dc82192c72ca9f1f1e9a3b3634e876f7b5d
Nas <1bestany@gmail.com>
2023-09-04 22:10:07 +0400

    added homeworktask41

---
+++ task41/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+Console.Clear();
+
+Console.Write("Задайте длинну массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Задайте массив:");
+int[] FillUserArray(int arrayLength)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("Массив пользователя: [");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] userArray = FillUserArray(m);
+
+int PozitivCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.ForegroundColor = ConsoleColor.Blue;
+PrintArray(userArray);
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Положительных чисел в массиве - {PozitivCount(userArray)}");
+Console.ForegroundColor = ConsoleColor.White;
