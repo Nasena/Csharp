fe118564c6f9169a269a606a9e51451fc0ccf6f3
Nas <1bestany@gmail.com>
2023-09-21 20:30:51 +0400

    added homework task54

---
+++ task54/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Clear();
+
+int[,] Get2DArray(int row, int column, int min, int max)
+{
+    int[,] result = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] SortingArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] userArray = Get2DArray(4, 4, 0, 10);
+Print2DArray(userArray);
+Console.WriteLine();
+
+
+Print2DArray(SortingArray(userArray));
