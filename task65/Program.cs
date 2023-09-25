5f6a6a6c9f1834591dc546cbd289703feb6d26ba
Nas <1bestany@gmail.com>
2023-09-25 10:56:11 +0400

    added task65

---
+++ task65/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задайте значения M  и N. Напишите программу, которая выдает натуральные числа от M до N
+*/
+
+
+void NaturalNum(int m, int n)
+{
+    if (m<n)
+    {
+        System.Console.Write(m + ",");
+        NaturalNum(m+1, n);
+    }
+}
+
+NaturalNum(1,5);
