2c7db72e053f205240749246e62f1d8d26fff97b
Nas <1bestany@gmail.com>
2023-09-25 10:52:31 +0400

    added task63

---
+++ task63/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задайте значение N. 
+Напишите программу, которая выдает все натуральные числа в промежутке от од 1 до N
+*/
+
+void NaturalNum(int n, int start = 1)
+{
+    if (start <= n)
+    {
+        System.Console.Write(start + ",");
+        NaturalNum(n, start+1);
+    }
+
+}
+
+NaturalNum(5);
