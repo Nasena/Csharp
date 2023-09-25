81c26389e3d474c648934e3386944fc2d7723547
Nas <1bestany@gmail.com>
2023-09-25 11:08:07 +0400

    added task67

---
+++ task67/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая будет принимать на вход  число и возвращать сумму его цифр.
+*/
+
+int result = SumNumber(256);
+int SumNumber(int n)
+{
+    if (n == 0) return 0;
+    else
+        return n % 10 + SumNumber(n / 10);
+}
+
+System.Console.Write(result);
