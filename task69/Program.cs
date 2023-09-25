5c7a17bfe30721ae3f88b304b792da11c53ecca7
Nas <1bestany@gmail.com>
2023-09-25 11:13:28 +0400

    added task69

---
+++ task69/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа A и B, и возводит число в целую степень B.
+
+*/
+
+int result = ProdNumber(2,4);
+int ProdNumber(int a, int b)
+{
+    if (b == 1) return a;
+    else
+    return a*ProdNumber(a, b-1);
+}
+
+System.Console.Write(result);
